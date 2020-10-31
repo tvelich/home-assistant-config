4563f271e4b7d9d19220c783180009cde6f5ac4a
Tom Velich <thomas.velich@gmail.com>
2020-10-31 13:57:27 -0400

    :wrench: Convert fireplace log warnings to info level

--- python_scripts/handle_fireplace.py
+++ python_scripts/handle_fireplace.py
@@ -2,6 +2,8 @@ REPEAT_TIMES = 5
 TEMP_UPPER_THRESHOLD = 3
 TEMP_LOWER_THRESHOLD = 0.5
 
+logger.setLevel('INFO')
+
 def call_service_repeat(action):
     for cycle in range(REPEAT_TIMES):
         hass.services.call('light', action, { 'entity_id': 'light.fireplace' }, True)
@@ -20,27 +22,27 @@ def main():
     is_target_temp_exceeded = (current_temp - target_temp) > TEMP_UPPER_THRESHOLD
 
     if is_target_temp_exceeded:
-        logger.warning('turning off fireplace due to excessive heat')
+        logger.warn('turning off fireplace due to excessive heat')
         hass.services.call('script', 'post_slack_message', { 'message':  'Fireplace turning off due to excessive heat' }, False)
         call_service_repeat('turn_off')
     elif is_fireplace_probably_on:
         if is_cool_mode:
-            logger.warning('turning off fireplace because cool mode')
+            logger.info('turning off fireplace because cool mode')
             call_service_repeat('turn_off')
         if not is_home:
-            logger.warning('turning off fireplace because not home')
+            logger.info('turning off fireplace because not home')
             call_service_repeat('turn_off')
         if is_sleep_mode:
-            logger.warning('turning off fireplace because sleep mode on')
+            logger.info('turning off fireplace because sleep mode on')
             call_service_repeat('turn_off')
         if not current_temp:
-            logger.warning('turning off fireplace because unknown current temp')
+            logger.info('turning off fireplace because unknown current temp')
             call_service_repeat('turn_off')
         elif is_heat_mode and is_target_temp_almost_reached:
-            logger.warning('turning off fireplace because target temp reached')
+            logger.info('turning off fireplace because target temp reached')
             call_service_repeat('turn_off')
     elif not is_fireplace_probably_on and is_heat_mode and is_temp_low_enough_to_turn_on and is_home and not is_sleep_mode:
-            logger.warning('turning on fireplace because below target temp')
+            logger.info('turning on fireplace because below target temp')
             call_service_repeat('turn_on')
 
 main()
