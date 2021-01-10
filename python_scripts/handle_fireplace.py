3d34001fc615dfcca3cd4dcab825fd54a973cc3e
Tom Velich <thomas.velich@gmail.com>
2021-01-09 23:17:06 -0500

    :wrench: Update handle fireplace script

--- python_scripts/handle_fireplace.py
+++ python_scripts/handle_fireplace.py
@@ -1,6 +1,6 @@
 REPEAT_TIMES = 5
 TEMP_UPPER_THRESHOLD = 3
-TEMP_LOWER_THRESHOLD = 0.5
+TEMP_LOWER_THRESHOLD = 0.3
 
 logger.setLevel('INFO')
 
@@ -21,6 +21,8 @@ def main():
     is_temp_low_enough_to_turn_on = (target_temp - current_temp) >= TEMP_LOWER_THRESHOLD
     is_target_temp_exceeded = (current_temp - target_temp) > TEMP_UPPER_THRESHOLD
 
+    current_minute = datetime.datetime.now().minute
+
     if is_target_temp_exceeded:
         logger.warn('turning off fireplace due to excessive heat')
         hass.services.call('script', 'post_slack_message', { 'message':  'Fireplace turning off due to excessive heat' }, False)
@@ -29,18 +31,21 @@ def main():
         if is_cool_mode:
             logger.info('turning off fireplace because cool mode')
             call_service_repeat('turn_off')
-        if not is_home:
+        elif not is_home:
             logger.info('turning off fireplace because not home')
             call_service_repeat('turn_off')
-        if is_sleep_mode:
+        elif is_sleep_mode:
             logger.info('turning off fireplace because sleep mode on')
             call_service_repeat('turn_off')
-        if not current_temp:
+        elif not current_temp:
             logger.info('turning off fireplace because unknown current temp')
             call_service_repeat('turn_off')
         elif is_heat_mode and is_target_temp_almost_reached:
             logger.info('turning off fireplace because target temp reached')
             call_service_repeat('turn_off')
+        elif current_minute == 0:
+            logger.info('sending fireplace keepalive')
+            call_service_repeat('turn_on')
     elif not is_fireplace_probably_on and is_heat_mode and is_temp_low_enough_to_turn_on and is_home and not is_sleep_mode:
             logger.info('turning on fireplace because below target temp')
             call_service_repeat('turn_on')
