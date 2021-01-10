fff38283dea057d2c066f1cad8480784894b897b
Tom Velich <thomas.velich@gmail.com>
2021-01-09 23:19:36 -0500

    :wrench: Update light scripts

--- python_scripts/light_turn_off.py
+++ python_scripts/light_turn_off.py
@@ -4,13 +4,16 @@ entity_id = data.get('entity_id')
 transition = data.get('transition', DEFAULT_TRANSITION)
 replay = data.get('replay', 'true') == 'true'
 
-if entity_id:
-    service_data = { 'entity_id': entity_id, 'transition': transition }
-    hass.services.call('light', 'turn_off', service_data, False)
-
-    # In case something failed to turn off, replay call after when the transition should be complete
-    if replay:
-        time.sleep(transition + 1)
+def main():
+    if entity_id:
+        service_data = { 'entity_id': entity_id, 'transition': transition }
         hass.services.call('light', 'turn_off', service_data, False)
-else:
-    logger.error('Missing entity id!')
+
+        # In case something failed to turn off, replay call after when the transition should be complete
+        if replay:
+            time.sleep(transition + 1)
+            hass.services.call('light', 'turn_off', service_data, False)
+    else:
+        logger.error('Missing entity id!')
+
+main()
--- python_scripts/light_turn_on.py
+++ python_scripts/light_turn_on.py
@@ -2,33 +2,38 @@ DEFAULT_KELVIN = 5000
 DEFAULT_BRIGHTNESS = 255
 DEFAULT_TRANSITION = 3
 
-entity_id = data.get('entity_id')
-kelvin = data.get('kelvin') or float(hass.states.get('input_number.default_kelvin').state) or DEFAULT_KELVIN
-color_name = data.get('color_name')
-effect = data.get('effect')
-brightness = data.get('brightness', DEFAULT_BRIGHTNESS)
-transition = data.get('transition', DEFAULT_TRANSITION)
-preserve_brightness = data.get('preserve_brightness')
-replay = data.get('replay', 'true') == 'true'
+def main():
+    entity_id = data.get('entity_id')
+    kelvin = data.get('kelvin') or float(hass.states.get('input_number.default_kelvin').state) or DEFAULT_KELVIN
+    color_name = data.get('color_name')
+    effect = data.get('effect')
+    brightness = data.get('brightness', DEFAULT_BRIGHTNESS)
+    transition = data.get('transition', DEFAULT_TRANSITION)
+    preserve_brightness = data.get('preserve_brightness')
+    replay = data.get('replay', True)
+    preserve_color = data.get('preserve_color', False)
 
-if entity_id:
-    service_data = { 'entity_id': entity_id, 'transition': transition }
+    if entity_id:
+        service_data = { 'entity_id': entity_id, 'transition': transition }
 
-    if brightness and not preserve_brightness:
-        service_data['brightness'] = brightness
+        if brightness and not preserve_brightness:
+            service_data['brightness'] = brightness
 
-    if color_name:
-        service_data['color_name'] = color_name
-    elif effect:
-        service_data['effect'] = effect
-    else:
-        service_data['kelvin'] = kelvin
+        if not preserve_color:
+            if color_name:
+                service_data['color_name'] = color_name
+            elif effect:
+                service_data['effect'] = effect
+            else:
+                service_data['kelvin'] = kelvin
 
-    hass.services.call('light', 'turn_on', service_data, False)
-
-    # In case something failed to turn on, replay after when the transition should be complete
-    if replay:
-        time.sleep(transition + 1)
         hass.services.call('light', 'turn_on', service_data, False)
-else:
-    logger.error('Missing entity id!')
+
+        # In case something failed to turn on, replay after when the transition should be complete
+        if replay:
+            time.sleep(transition + 1)
+            hass.services.call('light', 'turn_on', service_data, False)
+    else:
+        logger.error('Missing entity id!')
+
+main()
