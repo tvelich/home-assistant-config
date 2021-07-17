775eba11263259fc2e992270e73f28add6a994de
Tom Velich <thomas.velich@gmail.com>
2021-07-17 19:59:40 -0400

    :sparkles: Add partial sleep mode

--- python_scripts/handle_fireplace.py
+++ python_scripts/handle_fireplace.py
@@ -13,7 +13,7 @@ def main():
     is_heat_mode = hass.states.is_state('sensor.hallway_thermostat_operation_mode', 'heat')
     is_fireplace_probably_on = hass.states.is_state('light.fireplace', 'on')
     is_home = hass.states.is_state('binary_sensor.home', 'on')
-    is_sleep_mode = hass.states.is_state('input_boolean.sleep_mode', 'on')
+    is_sleep_mode = hass.states.is_state('sensor.sleep_mode', 'on')
     target_temp = float(hass.states.get('input_number.fireplace_temp').state)
     current_temp = float(hass.states.get('sensor.netatmo_home_indoor_temperature').state)
 
